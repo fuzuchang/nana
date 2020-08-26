08496ccd64a692ece1813e0705cfd67082dbad58
fuzuchang <fuzuchang@youxuezhishang.com>
2020-08-26 17:59:10 +0800

    修改bin

--- main.go
+++ main.go
@@ -4,14 +4,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type User struct {
+	ID   uint64
+	Name string
+}
+
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 
-		c.JSON(200, gin.H{
-			"hello": "this hello",
-			"world": "this world",
-		})
+		users := []User{{ID: 123, Name: "张三"}, {ID: 456, Name: "李四"}}
+
+		c.JSON(200, users)
 
 	})
 	r.Run(":7999")
